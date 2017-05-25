1f2999cf6d7a2a0a520aa4566adc5af640ced3ca
Lincoln Wisely <lincoln.wisely@manifest.com>
2017-05-25 13:46:27 -0500

    Initial react setup + first component

---
+++ app/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render()  {
+    return(
+      <div className="hero">
+      <h1>Webber-Wisely</h1>
+      <p>Its working!</p>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// import { render } from 'react-dom';
+
+// var App =
+//  require('./components/App');
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+
+);
