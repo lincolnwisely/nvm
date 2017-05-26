b09b5eeeccb7126ba1aa97793b24cf3b5294a1bd
Lincoln Wisely <lincoln.wisely@manifest.com>
2017-05-26 10:23:17 -0500

    Delete node_modules to add to git ignore

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import './style/style.css';
 // import { render } from 'react-dom';
 
 // var App =
@@ -9,5 +10,4 @@ import App from './components/App';
 ReactDOM.render(
   <App />,
   document.getElementById('app')
-
 );
