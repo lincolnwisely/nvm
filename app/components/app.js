b5ba48e4ffcb28bf8145fc4129fcf350f3c930b6
Lincoln Wisely <lincoln.wisely@manifest.com>
2017-11-13 10:26:07 -0600

    Remove unused component files and references

--- app/components/app.js
+++ app/components/app.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Navigation from './navigation.js';
 import Welcome from './welcome.js';
 import Details from './details.js';
-// import RSVP from './rsvp.js';
 import Form from './form.js';
 import Lodging from './lodging.js';
 import ToDo from './to-do.js';
 import Registry from './registry.js';
-// import Insta from './insta.js';
 import Footer from './footer.js';
 import style from '../style/scss/style.scss';
 
@@ -15,26 +13,22 @@ class App extends React.Component {
 
   render()  {
     return(
-      // <div id="outer-container">
         <div className="wrapper">
           <Navigation className="test" />
-          <div className="hero">
+          <div className="heading">
             <h1>Webber-Wisely</h1>
-            <p>---- March 30th, 2018 ----</p>
+            <p>Friday, March 30th, 2018</p>
+          </div>
+          <div className="hero">
           </div>
           <Welcome />
           <Details />
           <Form />
           <Lodging />
           <ToDo />
-
-          {/* <Insta /> */}
           <Registry />
           <Footer />
-
-
         </div>
-      // </div>
     );
   }
 }
